7b240d3dd1db851d010a29a79edb349b1369810e
liushanshan <liushanshan@csii.com.cn>
2018-05-23 15:10:54 +0800

    add credit page

---
+++ src/page/credit/main.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <main class="mt-3 mt-md-3">
+      <div class="row m-auto col-6 col-sm-6 col-md-3 col-lg-2 text-center bg-white br-primary">
+        <div class="col-12 col-md-12"><span class="font-weight-bold">累计学分：</span><span
+          class="red">34</span></div>
+        <div class="col-12 col-md-12"><span>学分排名：</span><span>34</span></div>
+      </div>
+      <div class="tab-pane">
+        <div class="tab-content">
+
+        </div>
+      </div>
+      <h6>学分来源</h6>
+      <table class="table table-bordered table-striped  bg-white text-center">
+        <thead class="table-primary ">
+        <tr>
+          <th scope="col">序号</th>
+          <th scope="col">课程名称</th>
+          <th scope="col">课程类型</th>
+          <th scope="col">所得学分</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>test01</td>
+          <td>test02</td>
+          <td>test03</td>
+          <td>test04</td>
+        </tr>
+        <tr>
+          <td>test01</td>
+          <td>test02</td>
+          <td>test03</td>
+          <td>test04</td>
+        </tr>
+        <tr>
+          <td>test01</td>
+          <td>test02</td>
+          <td>test03</td>
+          <td>test04</td>
+        </tr>
+        <tr>
+          <td>test01</td>
+          <td>test02</td>
+          <td>test03</td>
+          <td>test04</td>
+        </tr>
+        <tr>
+          <td>test01</td>
+          <td>test02</td>
+          <td>test03</td>
+          <td>test04</td>
+        </tr>
+        </tbody>
+      </table>
+    </main>
+  </div>
+</template>
+<script>
+  export default {
+    name:"credit"
+  }
+</script>
+<style>
+  @import "../../style/bootstrap.min.css";
+  html,body{
+    background-color: #f5f8fa;
+    height:100%;
+    width:100%;
+  }
+  input{
+    outline:none;
+  }
+  .red{
+    color:var(--red);
+  }
+  .br-primary{
+    border-radius: 1.5rem;
+  }
+</style>
+
---
+++ src/page/credit/welcome.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="credit-wrap">
+    <div id="credit" class="container col-md-6 col-11 col-sm-6">
+      <div class="title">学分管理</div>
+      <div class="row">
+        <div class="col-md-6 col-6 text-right ">选择导入文件</div>
+        <div class="col-md-6 col-6 text-left"><input type="file" name="fileName" id="fileId" class="col-12"></div>
+      </div>
+
+      <div class="row btn-box">
+        <div class="col-md-6 col-6 col-sm-6 text-right btn-bottom">
+          <button type="button" class="col-md-4 col-12 btn btn-success btn-group-lg">导入</button>
+        </div>
+        <div class="col-md-6 col-6 col-sm-6  text-left">
+          <button type="button" class="col-md-4 col-12 btn btn-success btn-group-lg">导出</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'welcome'
+  }
+</script>
+
+<style>
+  @import "../../style/bootstrap.min.css";
+
+  html,body{
+    background-color: #f5f8fa;
+    height:100%;
+    width:100%;
+  }
+  input{
+    outline:none;
+  }
+  .credit-wrap{
+    display: flex;
+    align-items: center;
+  }
+  #credit{
+    background-color: #fff;
+    border:1px solid #e4ecf3;
+    border-radius: 25px;
+  }
+
+  #credit .title {
+    font-size: 2.5rem;
+    font-weight: bold;
+    text-align: center;
+    padding-top:2rem;
+    padding-bottom:2rem;
+  }
+  #credit .btn-box{
+    padding-top:2rem;
+    padding-bottom:1rem;
+  }
+  .btn-bottom{
+    padding-bottom:1rem;
+  }
+
+</style>
